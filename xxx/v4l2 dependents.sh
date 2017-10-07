3c21fbeb72e300f4161b4aeb4c78190cde96fe22
xuan van tran <t.v.xuan94nd@gmail.com>
2017-10-07 17:54:35 +0700

    -----

---
+++ xxx/v4l2 dependents.sh
@@ -0,0 +1,5 @@
+/media/mmcblk0p2/usr/share/ti/syslink-examples/TI816X/notify/slaveloader startup VPSS-M3 /usr/share/ti/ti-media-controller-utils/dm816x_hdvpss.xem3
+insmod /lib/modules/2.6.37/kernel/drivers/video/ti81xx/vpss/vpss.ko
+insmod /lib/modules/2.6.37/kernel/drivers/media/video/tvp7002.ko
+insmod /lib/modules/2.6.37/kernel/drivers/media/video/ti81xx/ti81xxvin.ko
+ 
---
+++ xxx/xxx/Makefile
@@ -0,0 +1,8 @@
+# Import the variables from the EZSDK so that you can find the EZSDK components
+EZSDK=/home/l/Desktop/DM8168/ezsdk
+include ${EZSDK}/Rules.make
+ 
+test:
+# Make sure that you use a tab below
+	$(CSTOOL_PREFIX)gcc -Wall -o capture capture.c
+ 
